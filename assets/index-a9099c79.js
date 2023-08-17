c644e7c596cdedf1a8af4b8afc8a399545a60d5e
WA <sw955104@gmail.com>
2023-08-18 02:00:21 +0800

    Updates

---
+++ assets/index-a9099c79.js
@@ -0,0 +1,1 @@
+import"./main-ef9ed397.js";new Swiper(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",speed:1e4,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
--- assets/index-ee603c57.js
+++
@@ -1,1 +0,0 @@
-import"./main-9a0b1cd9.js";new Swiper(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",speed:1e4,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
