22874b627684516668e76c5e2d0368fcfc01ec31
WA <sw955104@gmail.com>
2023-09-05 17:18:51 +0800

    Updates

---
+++ assets/index-2125ac09.js
@@ -0,0 +1,1 @@
+import"./main-9b7d6916.js";new Swiper(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",speed:1e4,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
--- assets/index-644c7fd5.js
+++
@@ -1,1 +0,0 @@
-import"./main-679e918e.js";new Swiper(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",speed:1e4,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
